5c91db764533a6140757c106229777e7c970f425
qwet00 <36811940+qwet00@users.noreply.github.com>
2021-05-17 23:54:42 +0300

    Revert "push"
    This reverts commit 05decf7e876e7f1a188bbf71ba510ce93381fe9f.

--- src/screens/RegistrationScreen.js
+++ src/screens/RegistrationScreen.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useState,useEffect}  from 'react';
-import { StyleSheet, Text, View, ScrollView ,Image, Picker, AsyncStorage } from 'react-native';
+import { StyleSheet, Text, View, ScrollView ,Image, Picker} from 'react-native';
 import { FilledButton } from '../components/FilledButton';
 import { Heading } from '../components/Heading';
 import { Input } from '../components/Input';
@@ -13,20 +13,11 @@ import { API} from '../config/config';
 
 export function RegistrationScreen({navigation}) {
   const [selectedValue, setSelectedValue] = useState("Asistan");
-  const [email, setEmail] = useState("");
-  const [isim, setisim] = useState("");
-  const [sifre, setSifre] = useState("");
-  const [sifreTekrar, setSifreTekrar] = useState("");
-  const [placeholderr, setPlaceholderr] = useState("");
-
-    useEffect(() => {
-  AsyncStorage.getItem('name').then((value) =>
-         setPlaceholderr(value)
-       );
-  }, []);
-
+  const [email, setEmail] = useState("Asistan");
+  const [isim, setisim] = useState("Asistan");
+  const [sifre, setSifre] = useState("Asistan");
+  const [sifreTekrar, setSifreTekrar] = useState("Asistan");
     const kayit =()=> {
-
       if(!email.includes('@') || !email.includes('.'))
     alert("Lütfen geçerli bir e-mail adresi giriniz.");
 
@@ -117,10 +108,9 @@ export function RegistrationScreen({navigation}) {
         <Picker.Item label="Mudek-Yetkilisi" value="6" />
       </Picker>
       <Input style={styles.input}
-      placeholder={placeholderr}
+      placeholder={'Email'}
       keyboardType= "email-address"
       onChangeText={text => setEmail(text)}
-
       />
       <Input style={styles.input}
       placeholder={'İsim'}
